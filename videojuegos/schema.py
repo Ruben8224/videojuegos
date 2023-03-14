76f90d4b25bd17ef92449f8bdbf5bedb00e9a821
Ruben Castillo <Tartas_casr@hotmail.com>
2023-03-14 13:29:04 -0600

    create mutation

--- videojuegos/schema.py
+++ videojuegos/schema.py
@@ -6,5 +6,8 @@ import games.schema
 class Query(games.schema.Query, graphene.ObjectType):
     pass
 
+class Mutation(games.schema.Query, graphene.ObjectType):
+    pass
 
-schema = graphene.Schema(query=Query)
+
+schema = graphene.Schema(query=Query, mutation = Mutation)
